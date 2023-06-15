378743912cb599368d7d35740427677f4e2e271a
Abed Nganga <ngangaabed@gmail.com>
2023-06-15 23:41:22 +0300

    Completed Assignmrnt

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+// Code your solution in this file
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers (drivers){
+   const newArray = drivers.slice(0,2);
+   return newArray;
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const fareMultiplier = createFareMultiplier(2);
+function createFareMultiplier(multiplier){
+    return function (x){
+        return x * multiplier;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers);
+  }
